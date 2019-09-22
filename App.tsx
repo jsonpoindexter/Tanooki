6593720aa2c6d01ded2333c313d5a17155c1cb95
jason@zennify.com <HomeAfterHump2>
2019-09-21 18:01:45 -0700

    Implement Delta and filter for accel data

--- App.tsx
+++ App.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {Home} from './Pages/Home'
-
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Home } from './Pages/Home'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <Home />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingTop: 50,
   },
-});
+})
--- Pages/Home.tsx
+++ Pages/Home.tsx
@@ -1,32 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
-import {Accelerometer } from "expo-sensors";
+import React, { useState, useEffect } from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+import { Accelerometer, ThreeAxisMeasurement } from 'expo-sensors'
 import { Subscription } from 'react-native/Libraries/EventEmitter/NativeEventEmitter'
 
 export const Home = () => {
-    const [ currentAccelData, setAccelData ] = useState(null);
+  const [delta, setDelta] = useState(0.0)
+  let currentAccel = 0
+  let lastAccel = 0
+  let tempDelta = 0
 
-    useEffect(() => {
-        let subscription: Subscription | null = null;
-        const accelStatus = async() => {
-            try {
-                return await Accelerometer.isAvailableAsync();
-            } catch(_) {}
-        };
-        accelStatus().then((acceleState: boolean) => {
-            if(acceleState) {
-                Accelerometer.setUpdateInterval(16);
-                subscription = Accelerometer.addListener((data) => {
-                    setAccelData(data);
-                })
-            }
-        });
-        return () => {
-            Accelerometer.removeSubscription(subscription)
-        }
-    }, []);
+  useEffect(() => {
+    let subscription: Subscription | null = null
+    const accelStatus = async () => {
+      try {
+        return await Accelerometer.isAvailableAsync()
+      } catch (_) {}
+    }
 
-        return(
-            <Text>Status: { JSON.stringify(currentAccelData) }</Text>
-        )
+    const detectShake = (data: ThreeAxisMeasurement) => {
+      const x = data.x
+      const y = data.y
+      const z = data.z
+      lastAccel = currentAccel
+      currentAccel = Math.sqrt(x*x + y*y + z*z)
+      tempDelta = tempDelta * 0.9 + (currentAccel - lastAccel)
+      setDelta( Math.round(tempDelta)) // perform low-cut filter
+    }
+    accelStatus().then((accelState: boolean) => {
+      if (accelState) {
+        Accelerometer.setUpdateInterval(16)
+        subscription = Accelerometer.addListener((data: ThreeAxisMeasurement) => {
+          detectShake(data)
+        })
+      }
+    })
+    return () => {
+      Accelerometer.removeSubscription(subscription)
+    }
+  }, [])
+
+  return (
+    <View style={ delta > 3 ? style.red : null}>
+      <Text> delta: {delta} </Text>
+    </View>
+  )
 }
+const style = StyleSheet.create({
+  red: {
+    backgroundColor: 'red',
+  },
+})
+
